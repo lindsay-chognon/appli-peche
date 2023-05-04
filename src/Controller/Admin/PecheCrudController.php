b74f572ede17d57ab40851db62b78dfbb5301e61
lindsay <lindsaychognon@gmail.com>
2023-05-04 16:28:33 +0200

    Affichage des associations dans des listes déroulantes pour l'admin d'une pêche

--- src/Controller/Admin/PecheCrudController.php
+++ src/Controller/Admin/PecheCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Peche;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TimeField;
 
 class PecheCrudController extends AbstractCrudController
 {
@@ -12,14 +15,17 @@ class PecheCrudController extends AbstractCrudController
         return Peche::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield AssociationField::new('Pecheur');
+        yield AssociationField::new('Poisson');
+        yield AssociationField::new('Climat');
+        yield AssociationField::new('SessionPeche');
+        yield IntegerField::new('Temperature');
+        yield IntegerField::new('Taille');
+        yield TimeField::new('Heure');
+
     }
-    */
+
 }
